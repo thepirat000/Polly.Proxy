c2977160edc600e213b04edefe047071ec2b1ac7
thepirat000 <thepirat000@hotmail.com>
2019-02-27 19:02:41 -0600

    XML doc

--- PollyProxy/IPolicyConfig.cs
+++ PollyProxy/IPolicyConfig.cs
@@ -2,7 +2,15 @@
 {
     public interface IPolicyConfig
     {
+        /// <summary>
+        /// Uses the given sync policy
+        /// </summary>
+        /// <param name="policy">The policy to use</param>
         void Use(Policy policy);
+        /// <summary>
+        /// Uses the given async policy
+        /// </summary>
+        /// <param name="policy">The async policy to use</param>
         void Use(AsyncPolicy policy);
     }
 }
--- PollyProxy/IProxyConfig.cs
+++ PollyProxy/IProxyConfig.cs
@@ -9,20 +9,39 @@ namespace Polly.Proxy
     public interface IProxyConfig
     {
         /// <summary>
-        /// Maps a policy to 
+        /// Specifies a policy to be used under certain condition on the Method being called.
         /// </summary>
-        /// <param name="selector"></param>
-        /// <param name="policy"></param>
-        /// <returns></returns>
+        /// <param name="selector">A function of the MethodInfo that indicates a condition for matching this rule</param>
+        /// <param name="policy">The policy to use when the selector condition is satisfied</param>
         IProxyConfig When(Func<MethodInfo, bool> selector, IsPolicy policy);
+        /// <summary>
+        /// Specifies a policy to be used under certain condition on the Method being called.
+        /// </summary>
+        /// <param name="selector">A function of the MethodInfo that indicates a condition for matching this rule</param>
+        /// <param name="policyConfig">The policy configuration to use when the selector condition is satisfied</param>
         IProxyConfig When(Func<MethodInfo, bool> selector, Action<IPolicyConfig> policyConfig);
-
+        /// <summary>
+        /// Maps a policy for a method by its name.
+        /// </summary>
+        /// <param name="methodName">The method name for matching this rule (case-sensitive)</param>
+        /// <param name="policy">The policy to use</param>
         IProxyConfig For(string methodName, IsPolicy policy);
+        /// <summary>
+        /// Maps a policy for a method by its name.
+        /// </summary>
+        /// <param name="methodName">The method name for matching this rule (case-sensitive)</param>
+        /// <param name="policyConfig">The policy configuration</param>
         IProxyConfig For(string methodName, Action<IPolicyConfig> policyConfig);
-
+        /// <summary>
+        /// Defines a default Policy to use for any other method not mapped by other rules.
+        /// </summary>
+        /// <param name="policy">The policy to use as default</param>
         IProxyConfig Default(IsPolicy policy);
-
+        /// <summary>
+        /// Specifies the policies to be used as a function of the MethodInfo being called that returns the policy to use (or NULL).
+        /// </summary>
+        /// <param name="policyBuilder">A function of the MethodInfo that returns the policy to use. Can return NULL to indicate no-match.</param>
+        /// <returns></returns>
         IProxyConfig Map(Func<MethodInfo, IsPolicy> policyBuilder);
-        
     }
 }
